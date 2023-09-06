7e888ce8a2c219edf6cd17c623658a7cd97abc8d
Uday3421github <udaysonawane42@gmail.com>
2023-09-06 13:24:04 +0530

    first commit

--- SampleDemo/src/com/velocity/Sample.java
+++ SampleDemo/src/com/velocity/Sample.java
@@ -2,4 +2,10 @@ package com.velocity;
 
 public class Sample {
 
+	public static void main(String[] args) {
+		
+		for (int i=1; i<=5;i++) {
+			System.out.println(i);
+		}
+	}
 }
---
+++ SampleDemo/src/com/velocity/Test.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		System.out.println(al);
+	}
+
+}
